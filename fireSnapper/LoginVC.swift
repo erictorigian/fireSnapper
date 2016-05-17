a89f6df505a48e6bee14c344d3f39d380ce8a4b7
erictorigian <eric@torigian.com>
2016-05-17 15:22:15 -0400

    added email/password authentication

--- fireSnapper/LoginVC.swift
+++ fireSnapper/LoginVC.swift
@@ -61,4 +61,53 @@ class LoginVC: UIViewController {
 		
 	}
 	
+	@IBAction func attemptLogin(sender: UIButton! ) {
+		
+		if let email = emailField.text where email != "", let pwd = passwordField.text where pwd != "" {
+			
+			DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { error, authData in
+				if error != nil {
+					
+					if error.code == STATUS_ACCOUNT_NONEXISTS {
+						DataService.ds.REF_BASE.createUser(email, password: pwd, withValueCompletionBlock: { error, results in
+							if error != nil {
+								self.showErrorAlert("Could not create account", msg: "Problem creating account on server \(error.description)")
+							} else {
+								DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+								NSUserDefaults.standardUserDefaults().setValue(results[KEY_UID], forKey: KEY_UID)
+								self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+							}
+						})
+					} else if error.code == STATUS_INVALID_PWD {
+						self.showErrorAlert("Error logging in", msg: "Invalid password/email combination.   Please try again")
+						
+					} else 	{
+						self.showErrorAlert("Error logging in", msg: error.description)
+					}
+				}  else {
+					NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
+					self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+					
+				}
+			})
+			
+			
+		} else {
+			showErrorAlert("Login Error", msg: "You must include both an email and password to login")
+		}
+		
+	}
+
+	
+	
+	//MARK: - Other functions
+	
+	func showErrorAlert(title: String, msg: String) {
+		let alert = UIAlertController(title: title, message: msg, preferredStyle: .Alert )
+		let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+		alert.addAction(action)
+		presentViewController(alert, animated: true, completion: nil)
+	}
+	
+
 }
